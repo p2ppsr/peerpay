9626b3f4701a97b46f02f805d1f54ce64fc27d95
Jackie Lu <jackielu3@gmail.com>
2026-02-06 14:12:53 -0800

    rm developer fee

--- frontend/src/utils/peerPayClient.ts
+++ frontend/src/utils/peerPayClient.ts
@@ -4,10 +4,6 @@ import constants from './constants'
 import { WalletClient } from '@bsv/sdk'
 
 const babbageGo = new BabbageGo(new WalletClient(), {
-  monetization: {
-    developerFeeSats: 150,
-    developerIdentity: '02a064784ebb435e87c3961745b01e3564d41149ea1291d1a73783d1b7b3a7a220'
-  },
   walletUnavailable: {
     title: 'Instant, secure payments.'
   }
